4d64f6211b62359d74b46eca5e280b1221dc9250
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-06-26 16:37:29 +0100

    update: tested a link not working

--- src/utils/Links.js
+++ src/utils/Links.js
@@ -1,1 +1,13 @@
-export const links = [{ id: 1, name: "Home", href: "/" }];
+// "use server";
+// import { auth } from "@clerk/nextjs/server";
+// import { db } from "@/utils/dbConnection";
+// const { userId } = await auth();
+// const data = await db.query(`SELECT id FROM week09users WHERE uuid = $1`, [
+//   userId,
+// ]);
+// let id = 0;
+// if (data.rows.length > 0) id = data.rows[0].id;
+export const links = [
+  { id: 1, name: "Home", href: "/" },
+  //   { id: 2, name: "My Profile", href: `/user/${id}` },
+];
